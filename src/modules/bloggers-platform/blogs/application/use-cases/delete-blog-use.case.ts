c82d93212011441806f7ff7a98c3b236a062a183
iusmanof <iusmanof@gmail.com>
2026-03-22 05:21:39 +0300

    feat 1.3: DDD delete blog

--- src/modules/bloggers-platform/blogs/application/use-cases/delete-blog-use.case.ts
+++ src/modules/bloggers-platform/blogs/application/use-cases/delete-blog-use.case.ts
@@ -1,7 +1,5 @@
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import BlogsRepository from '../../infrastructure/blogs.repository';
-import { DomainException } from '@core/exceptions/filters/domain-exceptions';
-import { DomainExceptionCode } from '@core/exceptions/filters/domain-exception-codes';
 
 export class DeleteBlogCommand {
   constructor(public id: string) {}
@@ -12,13 +10,8 @@ export class DeleteBlogUseCase implements ICommandHandler<DeleteBlogCommand, voi
   constructor(private readonly blogsRepository: BlogsRepository) {}
 
   async execute({ id }: DeleteBlogCommand): Promise<void> {
-    const entity = await this.blogsRepository.delete(id);
-    if (!entity) {
-      throw new DomainException({
-        code: DomainExceptionCode.NotFound,
-        message: 'Blog not found',
-        extensions: [{ field: 'blog', message: 'Blog not found' }],
-      });
-    }
+    const blog = await this.blogsRepository.findByIdOrFail(id);
+    blog.delete();
+    await this.blogsRepository.delete(blog.getId());
   }
 }
